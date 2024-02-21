287df0c494c155a771f076bd1f98d4421eaf06af
Last <last7617@outlook.com>
2024-02-21 17:41:15 +0800

    first commit

---
+++ src/preload/index.d.ts
@@ -0,0 +1,6 @@
+declare global {
+    interface Window {
+        // TODO: define global interface
+        // context: {}
+    }
+}
---
+++ src/renderer/src/App.tsx
@@ -0,0 +1,9 @@
+const App = (): JSX.Element => {
+    return (
+        <>
+            <p>hello world!</p>
+        </>
+    )
+}
+
+export default App
